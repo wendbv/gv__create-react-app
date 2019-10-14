5365551ef903ba214cc96d57dde9eeb376e51722
Syberen van Munster <syberen@wend.nl>
2019-10-14 14:20:20 +0200

    add eslint hooks rules

--- packages/react-scripts/template-typescript/.eslintrc.js
+++ packages/react-scripts/template-typescript/.eslintrc.js
@@ -45,6 +45,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': ['off'],
     '@typescript-eslint/no-var-requires': ['off'],
     'react/prop-types': ['off'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'prettier/prettier': [
       'error',
       {
