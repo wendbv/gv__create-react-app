1576cbcdc176a512b64944d8d75f1c0e651d5db3
Syberen van Munster <syberen@wend.nl>
2019-10-30 09:49:15 +0100

    Change typescript declarations path

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -256,7 +256,7 @@ function verifyTypeScriptSetup() {
   if (!fs.existsSync(paths.appTypeDeclarations)) {
     fs.writeFileSync(
       paths.appTypeDeclarations,
-      `/// <reference types="react-scripts" />${os.EOL}`
+      `/// <reference types="@wend/gv-react-scripts" />${os.EOL}`
     );
   }
 }
